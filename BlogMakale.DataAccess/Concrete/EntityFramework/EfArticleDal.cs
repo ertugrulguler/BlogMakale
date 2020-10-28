f70ea1746be483d7cca268623ba737c483d9d0c6
Ertuğrul Güler <ertugrulguler@outlook.com.tr>
2020-10-29 00:00:19 +0300

    Context tanımı ve data access implementasyonu

--- BlogMakale.DataAccess/Concrete/EntityFramework/EfArticleDal.cs
+++ BlogMakale.DataAccess/Concrete/EntityFramework/EfArticleDal.cs
@@ -1,5 +1,6 @@
 ﻿using BlogMakale.Core.DataAccess.EntityFramework.Concrete;
 using BlogMakale.DataAccess.Abstract;
+using BlogMakale.DataAccess.Context;
 using BlogMakale.Entities.EntityClasses;
 using System;
 using System.Collections.Generic;
--- BlogMakale.DataAccess/Context/MakaleContext.cs
+++ BlogMakale.DataAccess/Context/MakaleContext.cs
@@ -1,10 +1,21 @@
-﻿using System;
+﻿using BlogMakale.Entities.EntityClasses;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace BlogMakale.DataAccess.Context
 {
-    public class MakaleContext
+    public class MakaleContext : DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-DCF48B5;Database=BlogMakaleDB;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Owner> Owners { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
