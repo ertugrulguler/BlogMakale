7e89c8e53aaf2bcdd7cb67147afef5e8846fbf16
Ertuğrul Güler <ertugrulguler@outlook.com.tr>
2020-10-28 23:17:16 +0300

    Core katmanının implementasyonu.

---
+++ BlogMakale.Core/DataAccess/EntityFramework/Abstract/IEntityRepository.cs
@@ -0,0 +1,17 @@
+﻿using BlogMakale.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace BlogMakale.Core.DataAccess.EntityFramework.Abstract
+{
+    public interface IEntityRepository<T> where T : class, IEntity, new()
+    {
+        List<T> GetList(Expression<Func<T, bool>> filter = null);
+        T Get(Expression<Func<T, bool>> filter);
+        T Add(T entity);
+        T Update(T entity);
+        void Delete(T entity);
+    }
+}
---
+++ BlogMakale.Core/Entities/IEntity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlogMakale.Core.Entities
+{
+    public interface IEntity
+    {
+    }
+}
