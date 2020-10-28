e8199bb916ad77ee87f9da9caccd1baa4dc0708b
Ertuğrul Güler <ertugrulguler@outlook.com.tr>
2020-10-28 23:49:51 +0300

    Business implementasyonu

---
+++ BlogMakale.Business/Abstract/IArticleService.cs
@@ -0,0 +1,15 @@
+﻿using BlogMakale.Entities.EntityClasses;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlogMakale.Business.Abstract
+{
+    public interface IArticleService
+    {
+        List<Article> GetAll();
+        Article GetById(int id);
+        Article Add(Article product);
+        Article Update(Article product);
+    }
+}
---
+++ BlogMakale.Business/Concrete/Managers/ArticleManager.cs
@@ -0,0 +1,37 @@
+﻿using BlogMakale.Business.Abstract;
+using BlogMakale.DataAccess.Abstract;
+using BlogMakale.Entities.EntityClasses;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlogMakale.Business.Concrete.Managers
+{
+    public class ArticleManager : IArticleService
+    {
+        private IArticleDal _articleDal;
+        public ArticleManager(IArticleDal articleDal)
+        {
+            _articleDal = articleDal;
+        }
+        public Article Add(Article product)
+        {
+            return _articleDal.Add(product);
+        }
+
+        public List<Article> GetAll()
+        {
+            return _articleDal.GetList();
+        }
+
+        public Article GetById(int id)
+        {
+            return _articleDal.Get(i => i.Id == id);
+        }
+
+        public Article Update(Article article)
+        {
+            return _articleDal.Update(article);
+        }
+    }
+}
