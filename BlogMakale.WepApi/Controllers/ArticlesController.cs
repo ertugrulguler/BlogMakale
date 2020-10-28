64c83d783f2a89aaa04ffc347a81b94badcfbd19
Ertuğrul Güler <ertugrulguler@outlook.com.tr>
2020-10-29 01:29:28 +0300

    Makale listesi dönen api eklendi.

---
+++ BlogMakale.WepApi/Controllers/ArticlesController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using BlogMakale.Business.Abstract;
+using BlogMakale.Entities.EntityClasses;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogMakale.WepApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArticlesController : ControllerBase
+    {
+        private IArticleService _articleService;
+        //private IMapper _mapper;
+        public ArticlesController(IArticleService articleService/*,IMapper mapper*/)
+        {
+            _articleService = articleService;
+            //_mapper = mapper;
+        }
+
+        [HttpGet("GetArticles")]
+        public IActionResult GetArticles()
+        {
+            var articles = _articleService.GetAll();
+            if (articles.Count == 0)
+                return Ok(new { Statu = false, Message = "Makale bulunamadı." });
+
+            //var response = _mapper.Map<List<ArticleQuery>>(articles);
+
+            return Ok(new { Status = true, articles });
+        }
+    }
+}
