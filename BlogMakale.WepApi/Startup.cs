aa88fc6bf6c10be5e6431d92c59f7177dc4aaf21
Ertuğrul Güler <ertugrulguler@outlook.com.tr>
2020-10-29 00:37:24 +0300

    initializerın startupa eklenmesi

--- BlogMakale.WepApi/Startup.cs
+++ BlogMakale.WepApi/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BlogMakale.DataAccess.DatabaseInitializer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -22,18 +23,17 @@ namespace BlogMakale.WepApi
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                Initializer.Seed();
             }
 
             app.UseHttpsRedirection();
