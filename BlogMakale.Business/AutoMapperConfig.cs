dbf20221ed0eb10f104b80e1758ba8a5ff2d0e79
Ertuğrul Güler <ertugrulguler@outlook.com.tr>
2020-10-29 01:28:26 +0300

    auto mapper config dosyası tanımlandı

---
+++ BlogMakale.Business/AutoMapperConfig.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using BlogMakale.Entities.EntityClasses;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlogMakale.Business
+{
+    class AutoMapperConfig:Profile
+    {
+        public AutoMapperConfig()
+        {
+            AddCategoryMapping();
+        }
+
+        private void AddCategoryMapping()
+        {
+            #region Article
+            CreateMap<Article, ArticleQuery>().ReverseMap();
+            CreateMap<Article, ArticleCommand>().ReverseMap();
+            #endregion
+
+            #region Owner
+            CreateMap<Owner, OwnerQuery>().ReverseMap();
+            CreateMap<Owner, OwnerCommand>().ReverseMap();
+            #endregion
+            
+            #region User
+            CreateMap<User, UserQuery>().ReverseMap();
+            CreateMap<User, UserCommand>().ReverseMap();
+            #endregion
+
+            #region Comment
+            CreateMap<Comment, CommentQuery>().ReverseMap();
+            CreateMap<Comment, CommentCommand>().ReverseMap();
+            #endregion
+        }
+    }
+}
